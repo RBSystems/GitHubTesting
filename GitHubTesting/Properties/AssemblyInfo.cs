19cc10dc089b3f0507fae143f407ff6099803409
Mark Allen <mark.allen@proav.com>
2020-03-25 15:47:00 +0000

    Initial commit

---
+++ GitHubTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("GitHubTesting")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitHubTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
